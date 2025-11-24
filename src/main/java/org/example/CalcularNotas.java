19630308d2cb92762bb37098f816001b05f07560
Matheus Monteiro de Moura <devmatheus12@gmail.com>
2025-11-24 15:43:45 -0300

    first commit

---
+++ src/main/java/org/example/CalcularNotas.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioHoraDaPratica {
+    public static void main(String[] args) {
+        List<Double> notas = List.of(8.0, 9.0, 3.0);
+        double sumNotas = notas.stream().reduce(0.0, (nota1, nota2) -> {
+            return nota1 + nota2;
+        });
+        double media = sumNotas / notas.toArray().length;
+
+        System.out.println(media);
+    }
+}
